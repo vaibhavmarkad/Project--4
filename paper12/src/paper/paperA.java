175653e1611689e01e347435aa421f47f8abbb2a
Vaibhav Annasaheb Markad <Lenovo@DESKTOP-28S5KMI>
2023-03-14 13:01:41 +0530

    add commit now

---
+++ paper12/src/paper/paperA.java
@@ -0,0 +1,28 @@
+package paper;
+
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class paperA {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: coder ");
+        String str = sc.nextLine();
+        Map<Character, Integer> occurrences = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            if (occurrences.containsKey(c)) {
+                occurrences.put(c, occurrences.get(c) + 1);
+            } else {
+                occurrences.put(c, 1);
+            }
+        }
+        System.out.println("Occurrences:");
+        for (Map.Entry<Character, Integer> entry : occurrences.entrySet()) {
+            System.out.println(entry.getKey() + "==> " + entry.getValue());
+        }
+        sc.close();
+    }
+
+}
